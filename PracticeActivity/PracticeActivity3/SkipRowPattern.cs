1d460f4883cf3279007eaae70e33137a6c96c007
DenreiAngeles <163831037+DenreiAngeles@users.noreply.github.com>
2025-05-21 14:54:09 +0800

    Create SkipRowPattern.cs

---
+++ PracticeActivity/PracticeActivity3/SkipRowPattern.cs
@@ -0,0 +1,29 @@
+using System;
+
+class SkipRowPattern
+{
+    static void Main()
+    {
+        Console.Write("Enter n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i == y)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
